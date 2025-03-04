e4cef114bf2ff9a514890353c1b24147528f7f4f
Fabrice <schaubfabrice@gmail.com>
2025-03-04 15:54:44 +0100

    external stubs

--- src/Compiler/stub.zig
+++ src/Compiler/stub.zig
@@ -1,22 +1,7 @@
 const Target = @import("Target.zig");
 const Machine = Target.Machine;
 
-const LINUX_X86_64_SYSV_STUB =
-    \\.section .note.GNU-stack,"",%progbits
-    \\.section .text  
-    \\.globl _start
-    \\.type _start, @function
-    \\_start:
-    \\    popq %rdi
-    \\    movq %rsp, %rsi
-    \\    andq $-16, %rsp
-    \\    subq $8, %rsp  
-    \\    call main
-    \\    addq $8, %rsp
-    \\    movq $60, %rax
-    \\    xorl %edi, %edi
-    \\    syscall
-;
+const LINUX_X86_64_SYSV_STUB = @embedFile("stubs/x86_64_sysv.S");
 
 fn getX86Specific(machine: Machine) []const u8 {
     switch (machine.os) {
