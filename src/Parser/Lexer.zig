91096c95e0374fd44a452dd44296e8567cbd8b78
Fabrice <schaubfabrice@gmail.com>
2025-03-18 22:43:14 +0100

    fixed unary

--- src/Parser/Lexer.zig
+++ src/Parser/Lexer.zig
@@ -139,21 +139,25 @@ fn lex(self: *Self) Token {
         '0'...'9' => return self.lexNumber(),
         'a'...'z', 'A'...'Z', '_' => return self.lexIdentifier(),
         '=' => return self.getTokenWithValue(TokenKind.Assign, .{ .Assign = OperatorType.Assign }),
-        '+', '-', '*', '/' => {
+        '+', '-' => {
             if (self.getChar() == '=') {
                 self.advance();
                 const op: OperatorType = if (operators.get(&[_]u8{c})) |op| op else unreachable;
                 return self.getTokenWithValue(TokenKind.Assign, .{ .Assign = op });
             }
 
+            if (ascii.isDigit(self.getChar())) {
+                return self.lexNumber();
+            }
+
             switch (c) {
                 '+' => return self.getToken(TokenKind.Plus),
                 '-' => return self.getToken(TokenKind.Minus),
-                '*' => return self.getToken(TokenKind.Star),
-                '/' => return self.getToken(TokenKind.Slash),
                 else => unreachable,
             }
         },
+        '*' => return self.getToken(TokenKind.Star),
+        '/' => return self.getToken(TokenKind.Slash),
         '.' => return self.getToken(TokenKind.Period),
         ':' => return self.getToken(TokenKind.Colon),
         '(' => return self.getToken(TokenKind.LParen),
